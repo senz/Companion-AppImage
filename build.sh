236f44faa440b0f0e135c3152dffd343d43b6e58
Kosta <kosta-codes@proton.me>
2024-05-20 18:33:52 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+chmod +x AppDir/AppRun
+
+BITFOCUS_COMPANION_URL="https://s3.bitfocus.io/builds/companion/companion-linux-x64-3.3.0+6991-stable-b27baf94.tar.gz"
+
+mkdir -p AppDir/companion
+if [ ! -f companion.tar.gz ]; then
+    echo "Download companion..."
+    curl -L $BITFOCUS_COMPANION_URL -o companion.tar.gz
+fi
+tar -xzf companion.tar.gz -C AppDir/companion --strip-components=1
+
+if ! command -v appimagetool.AppImage >/dev/null 2>&1
+then
+    echo "Download AppImage tool..."
+    LATEST_TOOL=$(curl -L "https://api.github.com/repos/AppImage/AppImageKit/releases/latest" | jq -r '.assets[] | select(.name | test("appimagetool-x86_64.AppImage$")) | .browser_download_url')
+    curl -L $LATEST_TOOL -o appimagetool.AppImage
+    chmod +x appimagetool.AppImage
+fi
+
+echo "Build AppImage..."
+if command -v appimagetool.AppImage >/dev/null 2>&1
+then ARCH=x86_64 appimagetool.AppImage -v AppDir
+else
+    ARCH=x86_64 ./appimagetool.AppImage --appimage-extract-and-run -v AppDir
+    rm ./appimagetool.AppImage
+fi
