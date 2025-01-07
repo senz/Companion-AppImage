27a6dfa0c1c19d4d9817aa0f2c39c39608ef47a7
Kosta <human@kosta.social>
2025-01-07 09:37:25 +0000

    chore: bump to 3.4.3

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 chmod +x AppDir/AppRun
 
-BITFOCUS_COMPANION_URL="https://s3.bitfocus.io/builds/companion/companion-linux-x64-3.4.1+7323-stable-e32a1052.tar.gz"
+BITFOCUS_COMPANION_URL="https://s3.bitfocus.io/builds/companion/companion-linux-x64-3.4.3+7347-stable-23764ac2.tar.gz"
 
 mkdir -p AppDir/companion
 if [ ! -f companion.tar.gz ]; then
